47e382f4a6bfb69e79974872d5b7f5e92fbd0407
LordAfterEight <elias@stettmayer.de>
2026-03-03 23:03:49 +0100

    Implement tokenizing and arg parsing

--- src/load_src.zig
+++ src/load_src.zig
@@ -5,23 +5,24 @@ const Memory = struct {
     mem_ptr: usize,
 };
 
-pub fn load_file(alloc: std.mem.Allocator, src_path: []const u8) anyerror![]u8 {
+/// Loads a file using a relative path.
+/// Returns a slice []u8 containing the raw bytes of the file
+///
+/// Can return various errors depending on whether the file cannot
+/// be opened or read or the file buffer or memory instance could
+/// not be initialized.
+pub fn load_src_file(alloc: std.mem.Allocator, src_path: []const u8) anyerror![]u8 {
     const file = std.fs.cwd().openFile(src_path, .{ .mode = .read_only }) catch |err| {
-        std.debug.print("\x1b[38;2;255;50;0mCould not open file at path: {s}\nError:\x1b[0m {s}\n", .{src_path, @errorName(err)});
+        std.debug.print("\x1b[38;2;255;50;0mCould not open file at path: {s}\nError:\x1b[0m {s}\n", .{ src_path, @errorName(err) });
         return err;
     };
     _ = &file;
 
-    var mem = alloc.create(Memory) catch |err| {
+    const mem = alloc.create(Memory) catch |err| {
         std.debug.print("\x1b[38;2;255;50;0mCould not create memory instance\nError:\x1b[0m {s}\n", .{@errorName(err)});
         return err;
     };
     mem.* = .{ .mem_ptr = 0 };
-
-    var used_space: usize = 0;
-
-    _ = &mem;
-    _ = &used_space;
 
     const buf: []u8 = alloc.alloc(u8, 65536) catch |err| {
         std.debug.print("\x1b[38;2;255;50;0mCould not initialize file buffer.\nError:\x1b[0m {s}\n", .{@errorName(err)});
@@ -33,6 +34,13 @@ pub fn load_file(alloc: std.mem.Allocator, src_path: []const u8) anyerror![]u8 {
         std.debug.print("\x1b[38;2;255;50;0mCould not read file\nError:\x1b[0m {s}\n", .{@errorName(err)});
         return err;
     };
-    std.debug.print("\x1b[38;2;50;255;50mRead {d} Bytes\nSRC:\x1b[0m", .{size});
+
+    if (size < 1024) {
+        std.debug.print("\x1b[38;2;50;255;50mRead {d} Bytes\nSRC:\x1b[0m", .{size});
+    } else if (size < 1024 * 1024) {
+        std.debug.print("\x1b[38;2;50;255;50mRead {d}KiB\nSRC:\x1b[0m", .{size / 1024});
+    } else if (size < 1024 * 1024 * 1024) {
+        std.debug.print("\x1b[38;2;50;255;50mRead {d}MiB\nSRC:\x1b[0m", .{size / 1024 / 1024});
+    }
     return buf;
 }
--- src/main.zig
+++ src/main.zig
@@ -4,26 +4,66 @@ const loading = @import("load_src.zig");
 
 var alloc = std.heap.c_allocator;
 
-
 pub fn main() void {
+    var src_path: []const u8 = "main.rvmasm";
+    var out_path: []const u8 = "ROM.bin";
+    var src_path_provided = false;
 
-    const src_path = "test.rvmasm";
+    var args = std.process.argsWithAllocator(alloc) catch return;
+    defer args.deinit();
+    _ = args.next();
 
-    const buf = loading.load_file(alloc, src_path) catch {
+    while (args.next()) |arg| {
+        if (std.mem.eql(u8, arg, "-s")) {
+            src_path = args.next() orelse {
+                std.debug.print("\x1b[38;2;255;50;0mMissing file path\x1b[0m\n", .{});
+                return;
+            };
+            src_path_provided = true;
+        } else if (std.mem.eql(u8, arg, "-o")) {
+            out_path = args.next() orelse "ROM.bin";
+        } else {
+            std.debug.print("\x1b[38;2;255;50;0mInvalid argument:\x1b[0m {s}\n", .{arg});
+            return;
+        }
+    }
+    if (!src_path_provided) {
+        std.debug.print("\x1b[38;2;255;200;50mWARNING: No src path provided. Looking for '\x1b[38;2;255;50;255mmain.rvmasm\x1b[38;2;255;200;0m'\x1b[0m\n", .{});
+    }
+
+
+    // ======== Loading ========
+
+    const buf = loading.load_src_file(alloc, src_path) catch {
         std.debug.print("\x1b[38;2;255;50;0mLoading failed\n", .{});
         return;
     };
 
-    var line: usize = 1;
-    std.debug.print("\n \x1b[38;2;255;200;50m{d}\x1b[0m  ", .{line});
-    line += 1;
+    var line_counter: usize = 1;
+    std.debug.print("\n \x1b[38;2;255;200;50m{d}\x1b[0m  ", .{line_counter});
+    line_counter += 1;
     for (buf) |byte| {
         switch (byte) {
             '\n', '\r' => {
-                std.debug.print("\n \x1b[38;2;255;200;50m{d}\x1b[0m  ", .{line});
-                line += 1;
+                std.debug.print("\n \x1b[38;2;255;200;50m{d}\x1b[0m  ", .{line_counter});
+                line_counter += 1;
             },
             else => std.debug.print("{c}", .{byte}),
         }
     }
+    std.debug.print("\n", .{});
+
+
+    // ======== Parsing ========
+
+    var lines = std.mem.tokenizeScalar(u8, buf, '\n');
+    while (lines.next()) |line| {
+        var tokens = std.mem.tokenizeScalar(u8, line, ' ');
+        while (tokens.next()) |token| {
+            std.debug.print("Token: {s}\n", .{token});
+        }
+        std.debug.print("\n", .{});
+    }
+
+    std.debug.print("{s}", .{out_path});
 }
